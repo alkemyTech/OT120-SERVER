7b187db9a8487366abd85e9a74d491cd71ca5d35
Florencia <75486164+flordife@users.noreply.github.com>
2021-12-16 20:19:32 -0300

    se elimina el archivo WebSecurityConfig.java duplicado y se agregan anotaciones en el RegistrationResponse. Se agrega tambien el permiso para la ruta /auth/**

--- src/main/java/com/alkemy/ong/model/response/RegistrationResponse.java
+++ src/main/java/com/alkemy/ong/model/response/RegistrationResponse.java
@@ -1,5 +1,10 @@
 package com.alkemy.ong.model.response;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class RegistrationResponse {
 
     public String username;
