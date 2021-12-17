1333ea1eec04e237840e62e9af346edce8b0d13f
cortinezjuan <cortinezjuan@gmail.com>
2021-12-17 14:45:36 -0300

    create endpoint DETALLE-CATEGORIA

--- src/main/java/com/alkemy/ong/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.service.CategoryServiceImpl;
 import com.alkemy.ong.service.abstraction.IDeleteCategoryService;
 import com.fasterxml.jackson.databind.introspect.TypeResolutionContext.Empty;
 import javax.persistence.EntityNotFoundException;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +18,22 @@ public class CategoryController {
   @Autowired
   private IDeleteCategoryService deleteCategoryService;
 
+  @Autowired
+  private CategoryServiceImpl categoryService;
+
   @DeleteMapping(value = "/categories/{id}")
   public ResponseEntity<Empty> delete(@PathVariable long id) throws EntityNotFoundException {
     deleteCategoryService.delete(id);
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+
+  @GetMapping("/categories/{id}")
+  public ResponseEntity<?> getOne(@PathVariable long id){
+    try{
+      return ResponseEntity.status(HttpStatus.OK).body(categoryService.getCategory(id));
+    }catch (Exception e){
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error. Category not found.\"}");
+    }
   }
 
 }
