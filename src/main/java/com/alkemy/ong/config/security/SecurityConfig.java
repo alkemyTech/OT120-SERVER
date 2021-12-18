81072868e5515641b5163a9a04cbb03686868537
Cortinezjuan <cortinezjuan@gmail.com>
2021-12-18 11:09:13 -0300

    permissions are added to test endpoint with postman

--- src/main/java/com/alkemy/ong/config/security/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/config/security/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
         .authorizeRequests()
+        .antMatchers("/categories/**").permitAll()
         .antMatchers(HttpMethod.DELETE, "/categories/**")
         .hasAnyRole(ApplicationRole.ADMIN.getName(), ApplicationRole.USER.getName())
         .antMatchers(HttpMethod.DELETE, "/users/**")
