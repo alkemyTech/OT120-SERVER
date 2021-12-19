1d73b4e355ccf911af59d04857fc6ded7546703f
Matias De la Fuente <mts.dlf@gmail.com>
2021-12-19 19:36:36 -0300

    modified service class, delete method deleted to meet ticket requirements

--- src/main/java/com/alkemy/ong/controller/OrganizationController.java
+++ src/main/java/com/alkemy/ong/controller/OrganizationController.java
@@ -4,36 +4,25 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alkemy.ong.model.request.OrganizationDto;
-import com.alkemy.ong.service.abstraction.IDeleteOrganizationService;
-import com.alkemy.ong.service.abstraction.IOrganizationDtoService;
+import com.alkemy.ong.dto.OrganizationDto;
+import com.alkemy.ong.service.abstraction.IOrganizationService ;
 
 @RestController
 @RequestMapping("organization/public")
 public class OrganizationController {
 
   @Autowired
-  private IOrganizationDtoService dtoService;	
-
-  @Autowired
-  private IDeleteOrganizationService softDeleteService;
+  private IOrganizationService organizationService;	
 
   @GetMapping("/{id}")
   public ResponseEntity<OrganizationDto> getOrganizationById(@Valid @PathVariable Long id) {
-    OrganizationDto organization = this.dtoService.getById(id);	
+    OrganizationDto organization = organizationService.getById(id);	
     return ResponseEntity.ok(organization);
-  }
-
-  @DeleteMapping
-  public ResponseEntity<OrganizationDto> delete(@Valid @PathVariable Long id) {
-    this.softDeleteService.delete(id);
-    return ResponseEntity.noContent().build();
   }
 
 }
