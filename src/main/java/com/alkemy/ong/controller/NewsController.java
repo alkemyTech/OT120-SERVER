f0ccdee3664587d258f95eb0501145107c7ed63f
Abel3581 <marzoa3581@gmail.com>
2021-12-20 14:24:37 -0300

    Correcciones

--- src/main/java/com/alkemy/ong/controller/NewsController.java
+++ src/main/java/com/alkemy/ong/controller/NewsController.java
@@ -4,7 +4,6 @@ import com.alkemy.ong.model.response.NewsResponseDto;
 import com.alkemy.ong.service.abstraction.INewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +17,6 @@ public class NewsController {
     INewsService newsService;
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<NewsResponseDto> getNewsById(@PathVariable Long id) {
         return ResponseEntity.ok(newsService.findNewsById(id));
     }
