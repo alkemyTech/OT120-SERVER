d93de52d02f3cfff959f4614c2050ac8fb4f4147
Cortinezjuan <cortinezjuan@gmail.com>
2021-12-22 12:56:33 -0300

    change USerDto fields from public to private

--- src/main/java/com/alkemy/ong/dto/UserDto.java
+++ src/main/java/com/alkemy/ong/dto/UserDto.java
@@ -15,18 +15,18 @@ public class UserDto {
 
     @NotBlank (message = "El nombre no puede estar vacío ni ser nulo")
     @Pattern(regexp = "[a-zA-Z]+", message = "El nombre no puede contener números")
-    public String firstName;
+    private String firstName;
 
     @NotBlank (message = "El apellido no puede estar vacío ni ser nulo")
     @Pattern(regexp = "[a-zA-Z]+", message = "El apellido no puede contener números")
-    public String lastName;
+    private String lastName;
 
     @NotBlank (message = "El email no puede estar vacío")
-    public String email;
+    private String email;
 
     @NotBlank (message = "La contraseña no puede estar vacía")
     @Size(min = 6, max = 25, message = "La contraseña debe ser entre 6 y 25 caracteres")
-    public String password;
+    private String password;
 
-    public String token;
+    private String token;
 }
