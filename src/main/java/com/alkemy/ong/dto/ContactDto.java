37d1184a9c768c30e46729e882e1995ae8f9737d
Florencia <75486164+flordife@users.noreply.github.com>
2021-12-29 14:22:18 -0300

    se agregan los campos al ContactDto

--- src/main/java/com/alkemy/ong/dto/ContactDto.java
+++ src/main/java/com/alkemy/ong/dto/ContactDto.java
@@ -1,4 +1,23 @@
 package com.alkemy.ong.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 public class ContactDto {
+
+    @NotEmpty
+    @Size(min= 3, max = 255)
+    public String name;
+
+    @Positive
+    @NotEmpty
+    @Size(min= 3, max = 255)
+    public Long phone;
+
+    @NotEmpty
+    @Size(min= 3, max = 255)
+    @Email
+    public String email;
 }
