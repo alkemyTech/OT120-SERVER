31d915dae2bcc656091fb44821440f6d085a2f11
mauroEO <mauroordonez@mi.unc.edu.ar>
2022-01-06 13:42:09 -0300

    Se agrega la clase MemberMapper con el metodo memberToDto

---
+++ src/main/java/com/alkemy/ong/mapper/MemberMapper.java
@@ -0,0 +1,16 @@
+package com.alkemy.ong.mapper;
+
+import com.alkemy.ong.dto.MemberDto;
+import com.alkemy.ong.model.entity.Member;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MemberMapper {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public MemberDto memberToDto (Member entity){
+        return modelMapper.map(entity, MemberDto.class);
+    }
+}
