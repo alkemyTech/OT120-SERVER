df265a5646531527aa343ded9cde11c3c195ee99
mauroEO <mauroordonez@mi.unc.edu.ar>
2022-01-06 14:00:38 -0300

    Se agrega endpoint /members, se agregaga anotacion a MemberMapper

--- src/main/java/com/alkemy/ong/controller/MemberController.java
+++ src/main/java/com/alkemy/ong/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.alkemy.ong.controller;
 
+import com.alkemy.ong.dto.MemberDto;
+import com.alkemy.ong.model.entity.Member;
 import com.alkemy.ong.service.abstraction.IMembersService;
 import com.fasterxml.jackson.databind.introspect.TypeResolutionContext.Empty;
 import javax.persistence.EntityNotFoundException;
@@ -7,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MemberController {
@@ -20,6 +25,11 @@ public class MemberController {
   public ResponseEntity<Empty> delete(@PathVariable long id) throws EntityNotFoundException {
     MembersService.delete(id);
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+
+  @GetMapping(value = "/members")
+  public  ResponseEntity<List<MemberDto>> getAllMember(){
+    return new ResponseEntity(MembersService.getAllMember(),HttpStatus.OK);
   }
 
 }
--- src/main/java/com/alkemy/ong/mapper/MemberMapper.java
+++ src/main/java/com/alkemy/ong/mapper/MemberMapper.java
@@ -4,7 +4,9 @@ import com.alkemy.ong.dto.MemberDto;
 import com.alkemy.ong.model.entity.Member;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MemberMapper {
 
     @Autowired
