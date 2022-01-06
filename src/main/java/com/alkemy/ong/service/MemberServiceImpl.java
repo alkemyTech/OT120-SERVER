2e86f88ed90908cd0027b3c37becaec2738e3479
mauroEO <mauroordonez@mi.unc.edu.ar>
2022-01-06 13:50:24 -0300

    Se agrega a MemberServiceImpl el metodo getAllMember

--- src/main/java/com/alkemy/ong/service/MemberServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.dto.MemberDto;
+import com.alkemy.ong.mapper.MemberMapper;
 import com.alkemy.ong.model.entity.Member;
 import com.alkemy.ong.repository.IMemberRepository;
 import com.alkemy.ong.service.abstraction.IMembersService;
+
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,11 +21,19 @@ public class MemberServiceImpl implements IMembersService {
   @Autowired
   private IMemberRepository memberRepository;
 
+  @Autowired
+  private MemberMapper memberMapper;
+
   @Override
   public void delete(Long id) throws EntityNotFoundException {
     Member member = getMember(id);
     member.setSoftDelete(true);
     memberRepository.save(member);
+  }
+
+  @Override
+  public List<MemberDto> getAllMember() {
+    return memberRepository.findAll().stream().map(memberMapper::memberToDto).collect(Collectors.toList());
   }
 
   private Member getMember(Long id) {
