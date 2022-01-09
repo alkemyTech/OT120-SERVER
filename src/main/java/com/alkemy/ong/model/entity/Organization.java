7e8360b596d4dfafd4571d33c80a309d21fbc90f
facupaez <facu.pz10@gmail.com>
2022-01-09 19:28:08 -0300

    conflicos solucionados

--- src/main/java/com/alkemy/ong/model/entity/Organization.java
+++ src/main/java/com/alkemy/ong/model/entity/Organization.java
@@ -23,7 +23,7 @@ import org.hibernate.annotations.Where;
 @Entity
 @SQLDelete(sql = "UPDATE organizations SET is_deleted=true WHERE id=?")
 @Where(clause = "is_deleted=false")
-@Table(name = "organizations")
+@Table(name = "organization")
 public class Organization {
 
   @Id
